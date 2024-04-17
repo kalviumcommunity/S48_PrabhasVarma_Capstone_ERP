7a3ce5dda171b44b199650bb70afca5168dcc90a
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-04-17 15:56:38 +0530

    updated routes.js file and connected the monogDB database name with the url

--- server/routes.js
+++ server/routes.js
@@ -1,29 +1,68 @@
 const express = require('express');
 const router = express.Router();
 
-let items = []; 
+let users = [
+    {
+        id:1,
+        name:"prabhas",
+        email:"prabhasvarma017@gmail.com"
+    }
+]; 
 
-router.post('/items', (req, res) => {
-    const newItem = req.body;
-    items.push(newItem);
-    res.status(201).json(newItem);
+// Create operation - POST
+router.post('/users', (req, res) => {
+    const {name,email} = req.body; 
+    if (!name || !email){ 
+        return res.status(404).json({message: "Required details are not provided"});
+    }
+    const newUser = {
+        id: users.length +1,
+        name: name,
+        email: email
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
-router.get('/items', (req, res) => {
-    res.json(items);
+// Read operation - GET
+router.get('/users', (req, res) => {
+    res.status(200).json(users);
+});
+// get req to read spcfic user data
+router.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id)   
+    const user = users.find(user => user.id === userId)
+    if (!user){
+        return res.status(404).json({
+            message:"User not found"
+        })
+    }
+    res.status(200).json(users);
+});
+// Update operation - PUT
+router.put('/users/:id', (req, res) => {
+    const userId =parseInt(req.params.id);
+    const {name,email} = req.body
+    const user = users.find(user=> user.id === userId)
+    if(!user){
+        return res.status(404).json({message:"User not found"})
+    }
+    user.name = name || user.name
+    user.email = email || user.email
+    
+    res.status(200).json(user);
 });
 
-router.put('/items/:id', (req, res) => {
-    const itemId = req.params.id;
-    const updatedItem = req.body;
-    items[itemId] = updatedItem;
-    res.json(updatedItem);
-});
-
-router.delete('/items/:id', (req, res) => {
-    const itemId = req.params.id;
-    items.splice(itemId, 1);
-    res.sendStatus(204);
+// Delete operation - DELETE
+router.delete('/users/:id', (req, res) => {
+    const userId =parseInt(req.params.id);
+    const index = users.findIndex(user=> user.id === userId)
+    if (index === -1){
+        return res.status(404).json({message:"User not found"})
+    }
+    users.splice(index, 1);
+    // res.sendStatus(204)
+    res.json("deleted item successfully");
 });
 
 module.exports = router;
