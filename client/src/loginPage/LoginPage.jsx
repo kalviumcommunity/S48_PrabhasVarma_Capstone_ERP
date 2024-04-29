9805bfb5ec612da58fff3c2a0eba52c7a73325e3
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-04-29 15:44:44 +0530

    established metamask connection

---
+++ client/src/loginPage/LoginPage.jsx
@@ -0,0 +1,38 @@
+// Login.page.js
+
+import React, { useState, useEffect } from 'react';
+import './LoginPage.css';
+import MetaMaskTatumButton from './Metamask/MetaMaskButton.jsx'
+import { useMetaMaskTatum } from './Metamask/useMetaMaskTatum.tsx';
+
+function Loginpage() {
+  const [isMetaMaskConnected, setIsMetaMaskConnected] = useState(false);
+  const { connectMetaMask, account } = useMetaMaskTatum();
+
+  const handleMetaMaskConnect = async () => {
+    await connectMetaMask();
+    setIsMetaMaskConnected(true);
+  };
+
+  useEffect(() => {
+    if (account) {
+      setIsMetaMaskConnected(true);
+    }
+  }, [account]);
+
+  return (
+    
+ <>{isMetaMaskConnected?
+  null
+ :
+
+     <MetaMaskTatumButton onConnect={handleMetaMaskConnect} />
+
+
+ }
+</>
+    
+  );
+}
+
+export default Loginpage;
